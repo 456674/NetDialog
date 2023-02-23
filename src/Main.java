e27750f0686c74cb35b3aa7e8640dcd8e0dafeb7
gaojack <2326117455@qq.com>
2023-02-23 17:47:33 +0800

    1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-
-
+        System.out.println();
         System.out.println("Hello world!");
     }
 }
